63e3f446fe6d20b2c3c87ec3bb04bd83f57cfff2
emel333 <marvinlee_3@outlook.com>
2026-06-28 17:58:52 -0500

    deleting aws resources

--- case/terraform/data.tf
+++ case/terraform/data.tf
@@ -10,8 +10,9 @@ resource "aws_db_instance" "app_db" {
   db_name             = "clarionflow"
   username            = var.db_username
   password            = random_password.db_password.result
-  skip_final_snapshot = true
+  skip_final_snapshot = false
   multi_az            = false
+  final_snapshot_identifier = "name"
 
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db.id]
